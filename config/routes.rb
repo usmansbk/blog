2d17fe7655f4f26fec7bf56d82e2048b517a8eae
Usman Suleiman <usmansbk@gmail.com>
2021-10-25 23:35:32 +0100

    Remove comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'users#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show] 
   end
