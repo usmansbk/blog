852da94fbe9f7ff31b4f0d2c0f017ee9d37b2902
Usman Suleiman <usmansbk@gmail.com>
2021-10-25 23:16:57 +0100

    Add user controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show] 
