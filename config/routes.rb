0b85f9ed7d774c5c247d4d024a3a92026634ea4b
Usman Suleiman <usmansbk@gmail.com>
2021-10-25 23:20:37 +0100

    Add post controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show] 
