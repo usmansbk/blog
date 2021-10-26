b0a8794128ed1215d4f0f74ed0ae13629ccd6ec5
Usman Suleiman <usmansbk@gmail.com>
2021-10-26 13:36:12 +0100

    Fix column name

--- db/migrate/20211026112550_add_user_ref_to_posts.rb
+++ db/migrate/20211026112550_add_user_ref_to_posts.rb
@@ -1,5 +1,5 @@
 class AddUserRefToPosts < ActiveRecord::Migration[6.1]
   def change
-    add_reference :posts, :user, null: false, foreign_key: { name: "author_id" } 
+    add_reference :posts, :user, null: false, foreign_key: true 
   end
 end
