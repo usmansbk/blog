c0cb1ce1a546f421d80321329a6e5ae14be134de
Usman Suleiman <usmansbk@gmail.com>
2021-10-26 01:44:47 +0100

    Test get index

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,1 +1,19 @@
 require 'rails_helper'
+
+RSpec.describe "Users", :type => :request do
+  describe 'GET #index' do
+    before(:example) { get users_path }
+
+    it 'should have correct response status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'should include correct placeholder text' do
+      expect(response.body).to include('Here is a list of users')
+    end
+  end
+end
