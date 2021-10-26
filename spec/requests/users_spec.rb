d7269e52044b494ac1abc1230ab0dddb7caa7580
Usman Suleiman <usmansbk@gmail.com>
2021-10-26 01:46:51 +0100

    Test get show route

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -16,4 +16,21 @@ RSpec.describe "Users", :type => :request do
       expect(response.body).to include('Here is a list of users')
     end
   end
+
+  describe 'GET #show' do
+    before(:example) { get user_path(1) }
+
+    it 'should have correct response status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'should include correct placeholder text' do
+      expect(response.body).to include('Here is a profile of a given user')
+    end
+
+  end
 end
