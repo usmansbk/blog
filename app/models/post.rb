c158229dac86de6ddf3567436c87f596fb761e4c
Usman Suleiman <usmansbk@gmail.com>
2021-10-31 16:01:14 +0100

    Create likes association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :users, through: :likes
+  has_many :likes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :posts, through: :likes
+  has_many :likes
 
   def recent_posts(limit = 3)
     posts.last(limit).order(created_at: :asc)
