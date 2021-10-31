43cf9d79cd95859e60f661ada37bb6c504567e2e
Usman Suleiman <usmansbk@gmail.com>
2021-10-31 16:05:14 +0100

    Cascade delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :likes
+  has_many :likes, dependent: :destroy
 
   def recent_posts(limit = 3)
     posts.last(limit).order(created_at: :asc)
