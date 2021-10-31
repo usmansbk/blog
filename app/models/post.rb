6668300249ae14eb81399bae330f68423ca2318b
Usman Suleiman <usmansbk@gmail.com>
2021-10-31 14:36:08 +0100

    Create user comment association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ApplicationRecord
-	belongs_to :user
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   def recent_posts(limit = 3)
     posts.last(limit).order(created_at: :asc)
