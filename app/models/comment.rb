e70419c20219c5a4d2f5c9354f239508550f9754
Usman Suleiman <usmansbk@gmail.com>
2021-10-31 14:40:01 +0100

    Fix association

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :user
-  has_many :posts, dependent: :destroy
+  belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
-  belongs_to :comment
+  has_many :comments, dependent: :destroy
 end
