820fba98251f762c6ba5ab1490b10c32cedf615b
Usman Suleiman <usmansbk@gmail.com>
2021-11-01 13:55:10 +0100

    generate avatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
   def get_photo(user)
-    user.photo.present? ? user.photo : 'https://via.placeholder.com/150'
+    user.photo.present? ? user.photo : "https://ui-avatars.com/api/?name=#{user.name}&background=random"
   end
 end
