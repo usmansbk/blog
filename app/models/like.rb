fc18bac891f48f7ca4ed30d4a5391fe4d96c04c0
Usman Suleiman <usmansbk@gmail.com>
2021-11-01 09:35:13 +0100

    Update counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post, counter_cache: :likes_counter
+
+  def update_counter(value)
+    post.update("likes_counter", value)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,8 @@ class Post < ApplicationRecord
   def recent_comments(limit = 5)
     comments.last(limit)
   end
+
+  def update_counter(value)
+    user.update("posts_counter", value)
+  end
 end
