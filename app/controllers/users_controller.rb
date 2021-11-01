ff445bacb7edb1bdb513e75a6003cf636b6f4d01
Usman Suleiman <usmansbk@gmail.com>
2021-11-01 10:12:30 +0100

    Implement show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,8 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.recent_posts
+  end
 end
