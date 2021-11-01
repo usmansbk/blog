1fbac4244851cd5c2c70dda2279302c905be836d
Usman Suleiman <usmansbk@gmail.com>
2021-11-01 22:09:02 +0100

    Create post new and create routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do 
-      resources :comments, only: [:new, :create]
-    end
+    resources :posts, only: [:index, :show]
   end
+
+  resources :posts, only: [:new, :create]
 end
