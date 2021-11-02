e65b0da81ea7dbe65a9328167e6419cefcca12f0
Usman Suleiman <usmansbk@gmail.com>
2021-11-02 01:41:12 +0100

    Create comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+  def create
+		post = current_user.posts.find(params[:post_id])
+    comment = post.comments.new(text: comment_params[:text], user: current_user)
+
+    respond_to do |format|
+      format.html do
+        if comment.save
+          redirect_to user_post_path(post.user.id, post.id), notice: 'Comment sent!'
+        else
+          flash.now[:error] = 'Faild to send comment!'
+          redirect_to user_post_path(post.user.id, post.id), alert: 'Failed to send comment!'
+        end
+      end
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:data).permit(:text)
+  end
 end
