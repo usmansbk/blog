f4db8477822a213548aa5544e5b2ffea02e48929
Usman Suleiman <usmansbk@gmail.com>
2021-11-02 02:24:54 +0100

    Create like record

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,7 @@
 class LikesController < ApplicationController
   def create
     post = Post.find(params[:post_id])
-		like = post.likes.new(user: current_user)
+    like = post.likes.new(user: current_user)
 
     respond_to do |format|
       format.html do
