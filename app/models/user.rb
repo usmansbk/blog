739975dc741460fe3db4553d44fef347d8bf9a93
Usman Suleiman <usmansbk@gmail.com>
2021-11-02 18:57:22 +0100

    Fix linter error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true 
+  validates :name, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
   has_many :posts, dependent: :destroy
