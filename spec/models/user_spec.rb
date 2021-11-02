b4f20fd242fc6dfa109bb63db37a3385d11ea0a1
Usman Suleiman <usmansbk@gmail.com>
2021-11-02 18:12:06 +0100

    Test user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validates' do
+    subject { User.new name: "Usman Suleiman" }
+
+    before { subject.save }
+    
+    it 'should have a name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'should have an alpha name' do
+      subject.name = ' '
+      expect(subject).to_not be_valid
+    end
+
+    it 'should have a postive integer posts counter' do
+      subject.posts_counter = -1
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe '#recent_posts' do
+    subject { User.first }
+
+    it 'should return 3 posts' do
+      expect(subject.recent_posts.length).to be(3)
+    end
+  end
 end
