84001902d8d10f6c006ba68676bd4ac899cd2ae2
Usman Suleiman <usmansbk@gmail.com>
2021-11-02 18:23:18 +0100

    Test post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validates' do
+    subject { Post.new title: "RSpec", user_id: 0 }
+
+    before { subject.save }
+    
+    it 'should have a title' do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'should have title with at most 250 characters' do
+      subject.title = 'RoR' * 250
+      expect(subject).to_not be_valid
+    end
+
+    it 'should not have a blank title' do
+      subject.title = ' '
+      expect(subject).to_not be_valid
+    end
+
+    it 'should have a postive integer comments counter' do
+      subject.comments_counter = -1
+      expect(subject).to_not be_valid
+    end
+
+    it 'should have a postive integer likes counter' do
+      subject.likes_counter = -1
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe '#recent_comments' do
+    subject { Post.first }
+
+    it 'should return 3 posts' do
+      expect(subject.recent_comments.length).to be(5)
+    end
+  end
+
+  describe '#update_counter' do
+    subject { Post.first }
+
+    it 'should update the comments counter' do
+      subject.update_counter(2)
+      expect(subject.user.posts_counter).to be(2)
+    end
+  end
 end
