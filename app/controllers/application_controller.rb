246953cf9819f7c2535e9d2e282658cad7d229e7
Usman Suleiman <usmansbk@gmail.com>
2021-11-03 02:31:35 +0100

    Protect routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery prepend: true
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   protected
 
