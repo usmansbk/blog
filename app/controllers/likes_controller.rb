1ed31e355eaae35f0a2435876852f7df569bf03f
Usman Suleiman <usmansbk@gmail.com>
2021-11-03 12:53:51 +0100

    Fix like and unlike

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,7 @@
 class LikesController < ApplicationController
   def create
     post = Post.find params[:post_id]
-    like = post.liked_by.new user: current_user
+    like = post.likes.new user: current_user
 
     respond_to do |format|
       format.html do
@@ -16,6 +16,7 @@ class LikesController < ApplicationController
 
   def destroy
     post = Post.find params[:id]
+    post.users.delete current_user
 
     redirect_to user_post_path(post.user.id, post.id), notice: 'Unliked!'
   end
