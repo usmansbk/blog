9fce8f7139e258d819ec4344d90b0a707d601df7
Usman Suleiman <usmansbk@gmail.com>
2021-11-03 11:07:45 +0100

    Add post like validation

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post, counter_cache: :likes_counter
+  validates :user_id, uniqueness: { scope: :post_id }
 
   def update_counter(value)
     post.update(likes_counter: value)
