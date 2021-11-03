5844c4868d9f164321d3478ac21cfee5c1d5656f
Usman Suleiman <usmansbk@gmail.com>
2021-11-03 20:24:01 +0100

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,12 @@ Rails.application.routes.draw do
     resources :posts, only: [:index, :show]
   end
 
-  resources :posts, only: [:new] do
+  resources :posts, only: [:new, :destroy] do
     resources :comments, only: [:create]
     resources :likes, only: [:create]
   end
 
+  resources :comments, only: [:destroy]
   resources :likes, only: [:destroy]
 
   post '/posts/new', to: 'posts#create', as: 'create_post'
