1afb468135163bdce7815c036f2e80629f4b734b
Usman Suleiman <usmansbk@gmail.com>
2021-11-04 00:49:29 +0100

    Change listener

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,7 @@ Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("turbolinks:load", () => {
   (document.querySelectorAll(".notification .delete") || []).forEach(
     ($delete) => {
       const $notification = $delete.parentNode;
