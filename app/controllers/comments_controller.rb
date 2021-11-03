9a3bc41511ff40a0ed5be6e29f83d4a9a05b5368
Usman Suleiman <usmansbk@gmail.com>
2021-11-03 22:35:54 +0100

    Remote button

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       format.html do
         if comment.save
-          redirect_to user_post_path(post.user.id, post.id)
+          redirect_to user_post_path(post.user.id, post.id), notice: 'Comment added!'
         else
           flash.now[:error] = 'Faild to send comment!'
           redirect_to user_post_path(post.user.id, post.id), alert: 'Failed to add comment!'
