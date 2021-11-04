5e1fd154d63efd4569d2312cc482f5fc514cde9b
Usman Suleiman <usmansbk@gmail.com>
2021-11-04 13:28:32 +0100

    Remove unused method

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,11 +1,4 @@
 module ControllerMacros
-  def login_admin
-    before(:each) do
-      @request.env["devise.mapping"] = Devise.mappings[:admin]
-      sign_in FactoryBot.create(:admin) # Using factory bot as an example
-    end
-  end
-
   def login_user
     before(:each) do
 			user = User.first
