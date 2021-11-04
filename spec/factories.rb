8184b25b41fe6170418f26e2d1ac89ccd7748d5b
Usman Suleiman <usmansbk@gmail.com>
2021-11-04 20:39:05 +0100

    Lint

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,30 +1,30 @@
 FactoryBot.define do
-	sequence :email do |n|
-		"test#{n}@example.com"
-	end
+  sequence :email do |n|
+    "test#{n}@example.com"
+  end
 
-	factory :user do
-		name { "John Doe" }
-		email
-		password { "secret_token" }
-		password_confirmation { "secret_token" }
-		confirmed_at { Date.today }
-	end
+  factory :user do
+    name { 'John Doe' }
+    email
+    password { 'secret_token' }
+    password_confirmation { 'secret_token' }
+    confirmed_at { Date.today }
+  end
 
-	factory :post do
-		user
-		title { "Post" }
-		text { "Test post" }
-	end
+  factory :post do
+    user
+    title { 'Post' }
+    text { 'Test post' }
+  end
 
-	factory :comment do
-		user
-		post
-		text { "Test comment" }
-	end
+  factory :comment do
+    user
+    post
+    text { 'Test comment' }
+  end
 
-	factory :like do
-		post
-		user
-	end
+  factory :like do
+    post
+    user
+  end
 end
--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,7 +1,7 @@
 module ControllerMacros
   def login_user
     before(:each) do
-      @user = FactoryBot.create :user 
+      @user = FactoryBot.create :user
       sign_in @user
     end
   end
