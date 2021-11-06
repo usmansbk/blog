92f973afa1b2adc7c81379b9fe59e3eba0c7c0d1
Usman Suleiman <usmansbk@gmail.com>
2021-11-06 20:33:53 +0100

    Add scenarios

--- spec/features/user_post_index_spec.rb
+++ spec/features/user_post_index_spec.rb
@@ -1,6 +1,29 @@
 require 'rails_helper'
 
 RSpec.feature 'User post index page', type: :feature do
-  login_user
-  background { visit user_posts_path(@user.id) }
+  background do
+    @user = FactoryBot.create :user_with_posts
+    sign_in @user
+    visit user_posts_path(@user.id)
+  end
+
+  scenario 'I can see the profile picture' do
+    expect(find('.user-avatar') { |img| img[:src] == get_photo(@user) }).to be_present
+  end
+
+  scenario 'I can see the username' do
+    expect(page).to have_content @user.name
+  end
+
+  scenario 'I can see the number of posts' do
+    expect(page).to have_content "#{@user.posts_counter} Posts" 
+  end
+
+  scenario "I can see a post's title" do
+    expect(page).to have_content @user.posts[0].title 
+  end
+
+  scenario "I can see some of the post's body" do
+    expect(page).to have_content @user.posts[0].text.slice(0, 100) 
+  end
 end
