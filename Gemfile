bb8b44a5a008d1acbf133af4ab4670c8c96af820
Usman Suleiman <usmansbk@gmail.com>
2021-11-06 11:55:20 +0100

    Clear database after test

--- Gemfile
+++ Gemfile
@@ -55,6 +55,7 @@ end
 group :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '>= 3.26'
+  gem 'database_cleaner-active_record'
   gem 'selenium-webdriver'
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,7 @@ require 'devise'
 require 'capybara/rails'
 require_relative 'support/controller_macros'
 require_relative 'support/factory_bot'
+require_relative 'support/database_cleaner'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -41,7 +42,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
