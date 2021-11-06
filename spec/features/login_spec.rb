84cda2d202ccd04f2385590a40b999c716cb442c
Usman Suleiman <usmansbk@gmail.com>
2021-11-06 18:38:04 +0100

    Refactor

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -3,19 +3,19 @@ require 'rails_helper'
 RSpec.feature 'Logins', type: :feature do
   background { visit new_user_session_path }
 
-  scenario 'I see the email and password inputs and the "Submit" button' do
+  scenario 'Page has a login form' do
     expect(page.has_field?('user_email')).to be true
     expect(page.has_field?('user_password')).to be true
     expect(page.has_button?('Log in')).to be true
   end
 
-  context 'submit' do
-    scenario 'without filling in the email and password' do
+  context 'Submit' do
+    scenario 'without email and password' do
       click_button 'Log in'
       expect(page).to have_content 'Invalid Email or password'
     end
 
-    scenario 'after filling in the username and password with incorrect data' do
+    scenario 'with incorrect email and password' do
       within 'form' do
         fill_in 'user_email', with: 'user@example.com'
         fill_in 'user_password', with: 'password'
@@ -25,7 +25,7 @@ RSpec.feature 'Logins', type: :feature do
     end
 
     given(:other_user) { FactoryBot.create :user }
-    scenario 'after filling in the username and password with correct data' do
+    scenario 'with correct email and password' do
       within 'form' do
         fill_in 'user_email', with: other_user.email
         fill_in 'user_password', with: other_user.password
