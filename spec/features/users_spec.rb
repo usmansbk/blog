0045f56853194db1c43e23b79d0097f43cb5ca16
Usman Suleiman <usmansbk@gmail.com>
2021-11-06 16:28:20 +0100

    chore

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -3,38 +3,24 @@ require 'rails_helper'
 RSpec.feature 'Users', type: :feature do
   login_user
   background do
-    @users = FactoryBot.create_list :user, 3
+    @other_user = FactoryBot.create :user, name: 'Other user'
     visit root_path
   end
 
-  context 'index page' do
-    scenario 'I can see the username of all other users' do
-      @users.each do |user|
-        expect(page).to have_content user.name
-      end
-    end
+  scenario 'I can see the username of all other users' do
+    expect(page).to have_content @other_user.name
+  end
 
-    scenario 'I can see the profile picture of each user' do
-      within '.columns' do
-        @users.each do |user|
-          expect(find('img') do |img|
-                   img[:src] == "https://ui-avatars.com/api/?name=#{user.name}&background=random"
-                 end).to be_present
-        end
-      end
-    end
+  scenario 'I can see the profile picture of each user' do
+    expect(page.has_link?("https://ui-avatars.com/api/?name=#{@other_user.name}&background=random")).to be true
+  end
 
-    scenario 'I can see the number of posts each user has written' do
-      @users.each do |user|
-        expect(page).to have_content "#{user.posts_counter} Post"
-      end
-    end
+  scenario 'I can see the number of posts each user has written' do
+    expect(page).to have_content "#{@other_user.posts_counter} Post"
+  end
 
-    scenario "When I click on a user, I am redirected to that user's show page" do
-      within '.columns' do
-        find_link(href: user_path(@users[0].id)).click
-      end
-      expect(current_path).to eq user_path(@users[0].id)
-    end
+  scenario "When I click on a user, I am redirected to that user's show page" do
+    find_link(href: user_path(@other_user.id)).click
+    expect(current_path).to eq user_path(@other_user.id)
   end
 end
