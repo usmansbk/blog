a2ad487a14be2850e88999aeeb717d4c18e906f9
Usman Suleiman <usmansbk@gmail.com>
2021-11-06 19:20:54 +0100

    Add scenarios

--- spec/features/users_show_spec.rb
+++ spec/features/users_show_spec.rb
@@ -1,16 +1,25 @@
 require 'rails_helper'
 
-RSpec.feature 'Users', type: :feature do
-  login_user
-  background { visit user_path(@user.id) }
+RSpec.feature 'User show page', type: :feature do
+  background do 
+    @user = FactoryBot.create :user_with_posts
+    sign_in @user
+    visit user_path(@user.id)
+  end
 
-  scenario "I can see the user's profile picture" do
+  scenario "I can see the profile picture" do
     expect(find('img') { |img| img[:src] == get_photo(@user) }).to be_present
   end
+
   scenario 'I can see the username' do
     expect(page).to have_content @user.name
   end
+
   scenario 'I can see the number of posts' do
     expect(page).to have_content "#{@user.posts_counter} Post"
+  end
+
+  scenario 'I can see the bio' do
+    expect(page).to have_content @user.bio
   end
 end
