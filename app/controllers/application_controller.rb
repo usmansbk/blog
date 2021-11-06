14850aeda7feb7cf313e29984f66756eff25cacf
Usman Suleiman <usmansbk@gmail.com>
2021-11-07 00:49:50 +0100

    Add comment api route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  include Response
+  include ExceptionHandler
+
   protect_from_forgery prepend: true
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
