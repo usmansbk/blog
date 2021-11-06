9495b245f1216bb78b289976a464045029665072
Usman Suleiman <usmansbk@gmail.com>
2021-11-06 15:30:25 +0100

    Refactor

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,16 +1,14 @@
 require 'rails_helper'
 
 RSpec.feature 'Logins', type: :feature do
-  before :each do
+  background do
     visit new_user_session_path
   end
 
-  context 'visit page' do
-    scenario 'I see the email and password inputs and the "Submit" button' do
-      expect(page.has_field?('user_email')).to be true
-      expect(page.has_field?('user_password')).to be true
-      expect(page.has_button?('Log in')).to be true
-    end
+  scenario 'I see the email and password inputs and the "Submit" button' do
+    expect(page.has_field?('user_email')).to be true
+    expect(page.has_field?('user_password')).to be true
+    expect(page.has_button?('Log in')).to be true
   end
 
   context 'Failure' do
