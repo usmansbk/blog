ec203dce10e4d86db81825556e5031cc9da424bb
Usman Suleiman <usmansbk@gmail.com>
2021-11-06 17:10:12 +0100

    Add examples

--- spec/features/users_show_spec.rb
+++ spec/features/users_show_spec.rb
@@ -3,4 +3,16 @@ require 'rails_helper'
 RSpec.feature 'Users', type: :feature do
   login_user
   background { visit user_path(@user.id) }
+
+  scenario "I can see the user's profile picture" do
+    expect(find('img') do |img|
+             img[:src] == "https://ui-avatars.com/api/?name=#{@user.name}&background=random"
+           end).to be_present
+  end
+  scenario 'I can see the username' do
+    expect(page).to have_content @user.name
+  end
+  scenario 'I can see the number of posts' do
+    expect(page).to have_content "#{@user.posts_counter} Post"
+  end
 end
