35cd25cfb718350d47e47621202c0f93295671d0
Usman Suleiman <usmansbk@gmail.com>
2021-11-07 10:38:22 +0100

    Remove comment

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,6 +1,5 @@
 class Api::PostsController < ApplicationController
   load_and_authorize_resource
-  # skip_authorize_resource only: :all_posts
 
   def index
     @posts = Post.all.order('created_at')
