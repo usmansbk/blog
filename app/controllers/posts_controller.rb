488353c54f38f5bc8a42673738bdc88d4fc367e1
Usman Suleiman <usmansbk@gmail.com>
2021-11-07 09:53:52 +0100

    Lint

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   load_and_authorize_resource
-  skip_authorize_resource :only => :all_posts
+  skip_authorize_resource only: :all_posts
 
   def index
     @user = User.find params[:user_id]
