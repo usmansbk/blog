74697738ac0a5195972c8b58c3d6b6fe9932ef7d
Usman Suleiman <usmansbk@gmail.com>
2021-11-08 00:58:11 +0100

    Lint

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,19 +1,19 @@
 class Api::SessionsController < Devise::SessionsController
-	protect_from_forgery prepend: true
-	skip_before_action :verify_authenticity_token
-	respond_to :json
+  protect_from_forgery prepend: true
+  skip_before_action :verify_authenticity_token
+  respond_to :json
 
-	private
+  private
 
-	def respond_with(resource, _opts = {})
-		if resource.email && resource.type
-			render json: { data: { email: resource.email, type: resource.type.downcase } }
-		else
-			head :unauthorized
-		end
-	end
+  def respond_with(resource, _opts = {})
+    if resource.email && resource.type
+      render json: { data: { email: resource.email, type: resource.type.downcase } }
+    else
+      head :unauthorized
+    end
+  end
 
-	def respond_to_on_destroy
-		head :ok
-	end
+  def respond_to_on_destroy
+    head :ok
+  end
 end
