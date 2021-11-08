da2cdccd1440cc5efea36bac679571230ff0d741
Usman Suleiman <usmansbk@gmail.com>
2021-11-08 16:47:41 +0100

    Seperate models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
   validates :name, :email, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
@@ -16,5 +17,11 @@ class User < ApplicationRecord
 
   def recent_posts(limit = 3)
     posts.includes(:comments).order('created_at').last(limit)
+  end
+
+  before_create :add_jti
+
+  def add_jti
+    self.jti ||= SecureRandom.uuid
   end
 end
