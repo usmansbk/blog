c7faf34f1bad2ac1daf60b5c276ec4ab90902912
Usman Suleiman <usmansbk@gmail.com>
2021-11-08 22:20:12 +0100

    Remove

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  include ExceptionHandler
-
   protect_from_forgery prepend: true
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
