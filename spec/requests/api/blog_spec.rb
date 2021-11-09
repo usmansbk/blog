537f930073c5d67c8f25e2828e3a7bca42ea49a5
Usman Suleiman <usmansbk@gmail.com>
2021-11-09 14:21:49 +0100

    Test signin route

--- spec/requests/api/blog_spec.rb
+++ spec/requests/api/blog_spec.rb
@@ -1,4 +1,17 @@
 require 'swagger_helper'
 
-RSpec.describe 'api/blog', type: :request do
+RSpec.describe 'Blog API', type: :request do
+	path '/users/sign_in' do
+
+		post 'Sign in user' do
+			tags 'Auth'
+			consumes 'application/json'
+
+			response '200', 'logged in' do
+				user = FactoryBot.create :user
+				let(:user) { { email: user.email, password: user.password } }
+				run_test!
+			end
+		end
+	end
 end
