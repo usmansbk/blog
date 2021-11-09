cbba201f568593bf866aac8e62f93bd44714b5c1
Usman Suleiman <usmansbk@gmail.com>
2021-11-09 15:13:50 +0100

    Test api

--- spec/requests/api/comments_spec.rb
+++ spec/requests/api/comments_spec.rb
@@ -4,25 +4,31 @@ RSpec.describe 'Comments API', type: :request do
   login_user
   path '/api/posts/{post_id}/comments' do
     parameter name: 'post_id', in: :path, type: :string, description: 'post_id'
-    let(:post) { FactoryBot.create :post, user: @user }
+    let(:Authorization) { "Bearer #{@user.jti}" }
 
     get 'list comments' do
       security [bearer_auth: []]
       response 200, 'successful' do
-        let(:Authorization) { "Bearer #{@user.jti}" }
-        let(:post_id) { post.id }
+        let(:post_id) { FactoryBot.create(:post, user: @user).id }
         run_test!
       end
     end
 
-    # post 'create comment' do
-    #   security [bearer_auth: []]
-    #   response 200, 'successful' do
-    #     let(:Authorization) { "Bearer #{@user.jti}" }
-    #     let(:comment) { { text: "Hello" } }
-    #     let(:post_id) { post.id }
-    #     run_test!
-    #   end
-    # end
+    post 'create comment' do
+      security [bearer_auth: []]
+      consumes 'application/json'
+      parameter name: :comment, in: :body, schema: {
+        type: :object,
+        properties: {
+          text: { type: :string }
+        },
+        required: ['text']
+      }
+      response 201, 'successful' do
+        let(:comment) { { text: 'Text' } }
+        let(:post_id) { FactoryBot.create(:post, user: @user).id }
+        run_test!
+      end
+    end
   end
 end
